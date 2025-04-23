ceebd97828a508cc4f639bff57388e8e2f80a52b
developmet <development@kanalyticstech.com>
2025-04-22 19:23:38 -0500

    Add initial structure for customers, loans, and payments apps

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_api_key',
+    'apps.customers',
+    'apps.loans',
+    'apps.payments',
 ]
 
 MIDDLEWARE = [
